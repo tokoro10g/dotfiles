69f44a5613b6964666719c20b5825b4b9ce420e6
Tokoro <tokoro10g@tokor.org>
2014-09-24 16:15:37 +0900

    tiny fix for MATLAB over SSH

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,7 +1,9 @@
 import XMonad
 import XMonad.Config.Xfce
+import XMonad.Hooks.SetWMName
 
 main = xmonad xfceConfig
 	{ terminal		= "xfce4-terminal"
 	, modMask		= mod4Mask
+	, startupHook	= setWMName "LG3D"
 	}

a015bdb511b53666f8e75b80700834db4743329a
Tokoro <tokoro10g@tokor.org>
2015-02-24 14:54:48 +0900

    removed old config

--- .xmonad/xmonad.hs
+++
@@ -1,9 +0,0 @@
-import XMonad
-import XMonad.Config.Xfce
-import XMonad.Hooks.SetWMName
-
-main = xmonad xfceConfig
-	{ terminal		= "xfce4-terminal"
-	, modMask		= mod4Mask
-	, startupHook	= setWMName "LG3D"
-	}
